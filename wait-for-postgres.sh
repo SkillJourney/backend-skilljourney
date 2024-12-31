b11181cef7bb9042b4d50a293791a84d01e703ac
ImMaxence <maxence.bonnici2002@gmail.com>
2024-12-31 18:14:09 +0100

    update docker files v2

--- wait-for-postgres.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-while ! nc -z ${POSTGRES_HOST} ${POSTGRES_PORT}; do
-  echo "Attente de PostgreSQL... 🌺"
-  sleep 1
-done
-
-echo "PostgreSQL est disponible, lancement du backend ! ✅"
-exec node server.js
