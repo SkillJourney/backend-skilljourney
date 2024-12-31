a9e5f8a41dfbe8a67074326c4be4eaf1c52f8d56
ImMaxence <maxence.bonnici2002@gmail.com>
2024-12-31 22:13:32 +0100

    postman collection & authorize middleware role

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,14 +1,15 @@
 import express from "express"
 import { createJob, getJobsByUserId, getJobById, getAllJobs, updateJob, deleteJob } from "../controllers/jobController.js"
 import verifyToken from "../middlewares/verifyTokenMiddleware.js"
+import authorizeRole from "../middlewares/authorizeRoleMiddleware.js"
 
 const router = express.Router()
 
-router.post("/create-job", verifyToken, createJob)
-router.get("/get-jobs-by-user-id/:id", verifyToken, getJobsByUserId)
-router.get("/get-job-by-id/:id", verifyToken, getJobById)
-router.get("/get-all-jobs", verifyToken, getAllJobs)
-router.put("/update-job/:id", verifyToken, updateJob)
-router.delete("/delete-job/:id", verifyToken, deleteJob)
+router.post("/create-job", verifyToken, authorizeRole(["COMPANY", "ADMIN"]), createJob)
+router.get("/get-jobs-by-user-id/:id", verifyToken, authorizeRole(["USER", "COMPANY", "ADMIN"]), getJobsByUserId)
+router.get("/get-job-by-id/:id", verifyToken, authorizeRole(["USER", "COMPANY", "ADMIN"]), getJobById)
+router.get("/get-all-jobs", verifyToken, authorizeRole(["USER", "COMPANY", "ADMIN"]), getAllJobs)
+router.put("/update-job/:id", verifyToken, authorizeRole(["COMPANY", "ADMIN"]), updateJob)
+router.delete("/delete-job/:id", verifyToken, authorizeRole(["COMPANY", "ADMIN"]), deleteJob)
 
 export default router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { getAllUsers, getUserById, updateUser, deleteUser } from "../controllers/userController.js"
 import verifyToken from "../middlewares/verifyTokenMiddleware.js"
+import authorizeRole from "../middlewares/authorizeRoleMiddleware.js"
 
 const router = express.Router()
 
-router.get("/get-all-user", verifyToken, getAllUsers)
-router.get("/get-user-by-id/:id", verifyToken, getUserById)
-router.put("/update-user/:id", verifyToken, updateUser)
-router.delete("/delete-user/:id", verifyToken, deleteUser)
+router.get("/get-all-user", verifyToken, authorizeRole(["ADMIN"]), getAllUsers)
+router.get("/get-user-by-id/:id", verifyToken, authorizeRole(["USER", "COMPANY", "ADMIN"]), getUserById)
+router.put("/update-user/:id", verifyToken, authorizeRole(["USER", "COMPANY", "ADMIN"]), updateUser)
+router.delete("/delete-user/:id", verifyToken, authorizeRole(["USER", "COMPANY", "ADMIN"]), deleteUser)
 
 export default router
